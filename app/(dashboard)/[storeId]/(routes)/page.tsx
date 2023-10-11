1b8ee2c61c5161749d0b2eb3c806e9c11a398dc6
Olawale Otubu <waleoxy@gmail.com>
2023-10-11 22:53:24 +0100

    fix:typo

--- app/(dashboard)/[storeId]/(routes)/page.tsx
+++ app/(dashboard)/[storeId]/(routes)/page.tsx
@@ -25,7 +25,7 @@ const DashboardPage: React.FC<DashboardPageProps> = async ({ params }) => {
   return (
     <div className="flex flex-col">
       <div className="flex-1 space-y-4 p-8 pt-6">
-        <Heading title="Dashboard" description="An overview of youe store" />
+        <Heading title="Dashboard" description="An overview of your store" />
         <Separator />
         <div className="grid gap-4 grid-cols-3">
           <Card>
